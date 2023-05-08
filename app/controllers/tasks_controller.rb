358b24f7bf7d6a8cb2d2344db0e97d83be36fc86
Jessica Carvajal <jessicacarm23@gmail.com>
2023-05-08 17:02:02 +0100

    rails new

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,23 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def new
+    @task = Task.new
+  end
+
   def show
     @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.new(params[:task])
+    @task.save # Will raise ActiveModel::ForbiddenAttributesError
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description, :completed)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
   # Defines the root path route ("/")
   # root "articles#index"
-  get "tasks", to: "tasks#index"
-  get "tasks/:id", to: "tasks#show", as: :task
-
-  #get "tasks/:id", to: "tasks#find", as: :restaurant
 end
